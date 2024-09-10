8268d0c1dd4891f4e22737de498f95561fe55ba8
Karolina Opitek <kjopi193@learner.dvusd.org>
2024-09-10 11:35:07 -0700

    ok

--- 1.4/Fraction_Tester.java
+++ 1.4/Fraction_Tester.java
@@ -9,6 +9,8 @@ public class Fraction_Tester
         System.out.println(Frac1);
         
         Fraction Frac2 = new Fraction(1,4);
-        System.out.println(Frac1 + "x" + frac2 + "=" + Frac1.mult(Frac2));
+        System.out.println(Frac1 + "x" + Frac2 + "=" + Frac1.mult(Frac2));
     }
+    
+    
 }
